e096b69fef83b84ab10a5f4ce80506bc5236be14
juliencombattelli <julien.combattelli@hotmail.com>
2017-03-30 16:19:37 +0200

    Rename CommunicationProtocol into Hcp and update source files

---
+++ src/Helicopter/Hcp.cpp
@@ -0,0 +1,8 @@
+/*
+ * Hcp.cpp
+ *
+ *  Created on: 22 févr. 2017
+ *      Author: JulienCombattelli
+ */
+
+#include <Helicopter/Hcp.h>
---
+++ src/Helicopter/Hcp.h
@@ -0,0 +1,29 @@
+/*
+ * Hcp.h
+ *
+ *  Created on: 22 févr. 2017
+ *      Author: JulienCombattelli
+ */
+
+#ifndef HCP_H_
+#define HCP_H_
+
+#include <stdint.h>
+#include <stddef.h>
+
+enum HCP_FrameType
+{
+	FrameType_ManualRotorMain 	= 0x01,
+	FrameType_ManualRotorTail 	= 0x02,
+	FrameType_Initialization 	= 0x11,
+	FrameType_SignalRotorMain 	= 0x12,
+	FrameType_SignalRotorTail 	= 0x13,
+	FrameType_Start 			= 0x10,
+	FrameType_Stop 				= 0xFF
+};
+
+inline uint16_t HCP_toUint16(uint8_t buffer[]) { return buffer[1] << 8 | buffer[0]; }
+
+inline uint32_t HCP_toUint32(uint8_t buffer[]) { return buffer[3] << 24 | buffer[2] << 16 | buffer[1] << 8 | buffer[0]; }
+
+#endif /* HCP_H_ */
